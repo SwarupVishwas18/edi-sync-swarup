815b58138c7535add45797cbf0a8dc08fc4692dd
Swarup Vishwas <swarupvishwas000@gmail.com>
2024-04-04 13:08:07 +0530

    Basic Routed Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import Sidebar from './components/Sidebar'
 import Main from './components/Main'
+
 
 function App() {
 
   return (
     <>
+      <Routes>
+        <Route path='/login' element={<h1>Login</h1>}></Route>
+      </Routes>
       <Header />
+
       <main className='main-arena'>
         <Sidebar />
-        <Main />
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/profile' element={<h1>Profile</h1>} />
+        </Routes >
       </main>
     </>
   )
