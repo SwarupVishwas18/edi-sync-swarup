0af0cb7e19ba0814abc897208bc2237a9cd1c14a
Swarup Vishwas <swarupvishwas000@gmail.com>
2024-04-05 00:39:21 +0530

    Added 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import Dashboard from './components/Dashboard'
 import MainArena from './MainArena'
 import Login from './Login'
+import Oops from './Oops'
 
 
 
@@ -13,6 +14,7 @@ function App() {
         <Route path='/login' element={<Login />}></Route>
         <Route path='/' element={<MainArena element={<Dashboard />} />} />
         <Route path='/profile' element={<MainArena element={<h1>Profile</h1>} />} />
+        <Route path='*' element={<Oops />} />
       </Routes>
 
 
