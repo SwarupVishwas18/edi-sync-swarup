60a190578109a9a151b1dad6ec4c3aacef98635f
Swarup Vishwas <swarupvishwas000@gmail.com>
2024-04-04 23:56:49 +0530

    Login Page Front end Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Dashboard from './components/Dashboard'
 import MainArena from './MainArena'
+import Login from './Login'
+
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/login' element={<h1>Login</h1>}></Route>
+        <Route path='/login' element={<Login />}></Route>
         <Route path='/' element={<MainArena element={<Dashboard />} />} />
         <Route path='/profile' element={<MainArena element={<h1>Profile</h1>} />} />
       </Routes>
